6be336c43b1938b8d8f58e2ca4e66230d9b87283
Aline Garbin <liligarbin3@gmail.com>
2021-02-25 00:44:23 +0100

    ta dando tudo certo =)

--- js/script3.js
+++ js/script3.js
@@ -1,9 +1,16 @@
 // HTML 3
      
-var numAserExibido = 2;
-var tresNumRandom = 789;
+
+
 
 const todosObj =[
+    { 
+        number : "NULL",
+        audio : "zero.wav",
+        alternatives: [9,0,3],
+        rightAns: 0,
+
+    },
     { 
         number : "EIN",
         audio : "one.wav",
@@ -18,66 +25,75 @@ const todosObj =[
         rightAns: 2,   
     },
     {
-        number : "ZWAI",
-        audio : "two.wav",
-        alternatives: [3,2,6],
-        rightAns: 2,   
+        number : "DREI",
+        audio : "three.wav",
+        alternatives: [3,7,9],
+        rightAns: 3,   
     },
     {
-        number : "ZWAI",
-        audio : "two.wav",
-        alternatives: [3,2,6],
-        rightAns: 2,   
+        number : "VIER",
+        audio : "four.wav",
+        alternatives: [6,4,8],
+        rightAns: 4,   
     },
     {
-        number : "ZWAI",
-        audio : "two.wav",
-        alternatives: [3,2,6],
-        rightAns: 2,   
+        number : "FÜNF",
+        audio : "five.wav",
+        alternatives: [2,7,5],
+        rightAns: 5,   
     },
     {
-        number : "ZWAI",
-        audio : "two.wav",
-        alternatives: [3,2,6],
-        rightAns: 2,   
+        number : "SECHS",
+        audio : "six.wav",
+        alternatives: [2,6,8],
+        rightAns: 6,   
     },
     {
-        number : "ZWAI",
-        audio : "two.wav",
-        alternatives: [3,2,6],
-        rightAns: 2,   
+        number : "SIEBEN",
+        audio : "seven.wav",
+        alternatives: [7,5,1],
+        rightAns: 7,   
     },
     {
-        number : "ZWAI",
-        audio : "two.wav",
-        alternatives: [3,2,6],
-        rightAns: 2,   
+        number : "ACHT",
+        audio : "eight.wav",
+        alternatives: [4,8,3],
+        rightAns: 8,   
     },
     {
-        number : "ZWAI",
-        audio : "two.wav",
-        alternatives: [3,2,6],
-        rightAns: 2,   
+        number : "NEUN",
+        audio : "nine.wav",
+        alternatives: [9,2,7],
+        rightAns: 9,   
     },
     {
-        number : "ZWAI",
-        audio : "two.wav",
-        alternatives: [3,2,6],
-        rightAns: 2,   
+        number : "ZEHN",
+        audio : "ten.wav",
+        alternatives: [1,2,10],
+        rightAns: 10,   
     },
 
 ]
 // quando o jogador acertar o current question vamos somar 1, e chamar showtoplayer novamente 
-let currentQuestion = 1;
+let currentQuestion = Math.floor(Math.random() * 11);
 
 function showToPlayer (index){
     // shownToPlayer é um obj que contem as propriedades numb, audio, alt, rightAns
     const shownToPlayer = todosObj[index];
 
-//  escrever um numero no html
+    // escrever um numero no html
 
-    const numberHtml = `<h1>${shownToPlayer.number}</h1>`;
+    const numberHtml = `<h1> ${shownToPlayer.number}</h1>`;
     document.getElementById('topNumber').innerHTML = numberHtml;
+
+    const alternativa1js = `<h1>${shownToPlayer.alternatives[0]}</h1>`;
+    document.getElementById('alternativa1').innerHTML = alternativa1js;
+
+    const alternativa2js = `<h1>${shownToPlayer.alternatives[1]}</h1>`;
+    document.getElementById('alternativa2').innerHTML = alternativa2js;
+
+    const alternativa3js = `<h1>${shownToPlayer.alternatives[2]}</h1>`;
+    document.getElementById('alternativa3').innerHTML = alternativa3js;
 
     // tocar o audio referente ao numero mostrado na tela
     const audio = new Audio ();
@@ -85,8 +101,47 @@ function showToPlayer (index){
     audio.src = "../audios/"+shownToPlayer.audio;
     audio.play()
 
-    console.log (numberHtml);
+    const alternative1Btn = document.getElementById("alternativa1");
+    alternative1Btn.addEventListener("click", function() {
+        if (shownToPlayer.alternatives[0] === shownToPlayer.rightAns) {
+            
+            alternative1Btn.style.backgroundColor="green";
+        }
+        else if (shownToPlayer.alternatives[0] != shownToPlayer.rightAns){
+            alert("errou");
+        }
+    });
+
+    const alternative2Btn = document.getElementById("alternativa2");
+    alternative2Btn.addEventListener("click", function() {
+        if (shownToPlayer.alternatives[1] === shownToPlayer.rightAns) {
+            alternative2Btn.style.backgroundColor="green";
+            
+        }
+        else if (shownToPlayer.alternatives[1] != shownToPlayer.rightAns){
+            
+            alert("errou");
+        }
+    });
+
+        const alternative3Btn = document.getElementById("alternativa3");
+    alternative3Btn.addEventListener("click", function() {
+        if (shownToPlayer.alternatives[2] === shownToPlayer.rightAns) {
+            
+            alternative3Btn.style.backgroundColor="green";
+        }
+        else if (shownToPlayer.alternatives[2] != shownToPlayer.rightAns){
+            alert("errou");
+        }
+    });
+    
+
+
 };
+
+var alternativesNums = document.getElementById("alternativesNums");
+alternativesNums.style.display="none";
+
 
 
 
@@ -95,11 +150,14 @@ var playBtn = document.getElementById("playBtn");
 playBtn.addEventListener("click", function startGame() {
 
     showToPlayer(currentQuestion)
-
-//   console.log('funcao rodando');
-//   var textDiv1 = document.createElement('h1');
-//   textDiv1.setAttribute ('id', 'textoDiv1');
-//   textDiv1.innerText = numAserExibido;
-//   document.getElementById('randomShown').appendChild(textDiv1);
+    playBtn.style.display="none";
+    alternativesNums.style.display="flex";
+    
 
 });
+
+
+// Next steps trocar as cores do  HTML fontes
+// Logica de ganhar e perder fazer botao ficar verde quando ganha
+//Logica de tirar os que ja foram 
+
